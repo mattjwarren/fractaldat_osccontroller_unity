18ac583c1178f26aad90ec2945b555c371eaef85
matthew_j_warren <matthew_j_warren@hotmail.com>
2014-07-20 03:26:53 +0100

    end of saturday

--- FractalDat.py
+++ FractalDat.py
@@ -24,24 +24,26 @@ class Grid(object):
         self.data=[ [0 for _ in xrange(x)] for _ in xrange(y) ]
         
     def _render_to_text(self):
-        print '\n\n'
         for row in self.data:
             print [ int(n) for n in row ]
+        
     
     def _render_to_colormap(self):
-        plt.imshow(self.data, interpolation='nearest',cmap=cm.gist_rainbow)
+        #normalises the data first, which is a shame
+        #plt.imshow(self.data, interpolation='nearest',cmap=cm.gist_rainbow)
+        
+        #this one averages by nearest neighbours, i think....
+        plt.imshow(self.data,cmap=cm.gist_rainbow)
         plt.show()
        
     def render(self):
-        self._render_to_colormap()
-        #self._render_to_text()
+        #self._render_to_colormap()
+        self._render_to_text()
         
-    def make(self,coordinate,value):
+    def make(self,coordinate,value):#make? ino rite?
+        if value<0 or value>100:
+            print 'CLIPPING',coordinate,value
         self.data[coordinate.x][coordinate.y]=value
-        
-    def make_new(self,coordinate,value):
-        if self.data[coordinate.x][coordinate.y]==0:
-            self.make(coordinate,value)
             
     def get(self,coordinate):
         return self.data[coordinate.x][coordinate.y]
@@ -49,11 +51,11 @@ class Grid(object):
 class FractalHeightmap(object):
     '''populates a 'grid' with a fractal heightmap'''
     def __init__(self,grid,rng_seed,roughness,
-                 corner_seeds=[(0,100),(0,100),(0,100),(0,100)],
+                 corner_seed_ranges=[(20,80),(20,80),(20,80),(20,80)],
                  max_depth=3):
         self.grid=grid
         self.max_depth=max_depth
-        self._set_initial_corners(corner_seeds)
+        self._set_initial_corners(corner_seed_ranges)
         self.roughness=roughness
         self.generate_heightmap([Coord(0,0),
                                  Coord(self.grid.size_x-1,0),
@@ -61,9 +63,10 @@ class FractalHeightmap(object):
                                  Coord(self.grid.size_x-1,self.grid.size_y-1)],1
                                 )
 
-    def _set_initial_corners(self,corner_seeds):
-        tl,tr,bl,br=corner_seeds
-        seeds=[[tl,tr],[bl,br]]
+    def _set_initial_corners(self,corner_seed_ranges):
+        tl,tr,bl,br=corner_seed_ranges
+        seeds=[[tl,tr],
+               [bl,br]]
         for x_idx,x in enumerate([0,self.grid.size_x-1]):
             for y_idx,y in enumerate([0,self.grid.size_y-1]):
                 try:
@@ -71,15 +74,13 @@ class FractalHeightmap(object):
                     val=minval+(random.random()*(maxval-minval))
                 except ValueError:
                     val=seeds[x_idx][y_idx]
-                self.grid.make_new(Coord(x,y),val)
+                self.grid.make(Coord(x,y),val)
                 
     def generate_heightmap(self,corners,depth):
         '''corners = (Coord(),Coord(),Coord(),Coord() / tl/tr/bl/br'''
         if depth>self.max_depth: return
-        #as soon as the midpoint overlays a corner (integer maths) we stop recursing
-        
-        #
         tl,tr,bl,br=corners
+        #define center-center coords
         center=Coord(tl.x+((tr.x-tl.x)/2),tr.y+((br.y-tr.y)/2))
         #define edge center coordinates
         top_c=Coord(tl.x+((tr.x-tl.x)/2),tl.y)
@@ -88,6 +89,8 @@ class FractalHeightmap(object):
         bot_c=Coord(bl.x+((br.x-bl.x)/2),bl.y)
         
         #calc the center and edge_center heights
+        
+        #center
         if self.grid.get(center)==0:
             avg=sum([self.grid.get(tl),
                     self.grid.get(tr),
@@ -95,9 +98,12 @@ class FractalHeightmap(object):
                     self.grid.get(br)]
                     )/4.0  ###NOTE, we can choose to use the current corners, the new edge-centers, or all
                     #currenty we use the current corners
-                    #then do the edge centers
-            offset=((random.random())-.5)*self.roughness 
-            self.grid.make_new(center,avg+offset)
+                    #then do the edge centers using only the edge corners, not the new center
+            offset=((random.random())-.5)*self.roughness #'they' have mentioned needing a scaled constant 
+                                                         #(inverse scaled to the  the depth of recurion)
+                                                         #scaling down results in landscape smoothing out in the cracks,
+                                                         #fairly good approx behaviour for water erosion
+            self.grid.make(center,avg+offset)
         
         #top_c
         if self.grid.get(top_c)==0:
@@ -105,7 +111,7 @@ class FractalHeightmap(object):
                     self.grid.get(tr)]
                     )/2.0
             offset=((random.random())-.5)*self.roughness
-            self.grid.make_new(top_c,avg+offset)
+            self.grid.make(top_c,avg+offset)
         
         #left_c
         if self.grid.get(left_c)==0:
@@ -113,7 +119,7 @@ class FractalHeightmap(object):
                      self.grid.get(bl)]
                     )/2.0
             offset=((random.random())-.5)*self.roughness
-            self.grid.make_new(left_c,avg+offset)
+            self.grid.make(left_c,avg+offset)
         
         #right_c
         if self.grid.get(right_c)==0:  
@@ -121,7 +127,7 @@ class FractalHeightmap(object):
                      self.grid.get(br)]
                     )/2.0
             offset=((random.random())-.5)*self.roughness
-            self.grid.make_new(right_c,avg+offset)
+            self.grid.make(right_c,avg+offset)
             
         #bot_c
         if self.grid.get(bot_c)==0:
@@ -129,7 +135,7 @@ class FractalHeightmap(object):
                      self.grid.get(br)]
                     )/2.0
             offset=((random.random())-.5)*self.roughness
-            self.grid.make_new(bot_c,avg+offset)
+            self.grid.make(bot_c,avg+offset)
 
         #g.render()
         self.generate_heightmap((tl,top_c,left_c,center),depth+1)
@@ -143,27 +149,32 @@ class FractalHeightmap(object):
 
 
 if __name__ == '__main__':
-    g_size=16#//must be power of 2
-    roughness=3.0
+    g_size=16#//must(n't) be a power of 2
+    roughness=7.0
+    target_rate=50
     
     test_ip=IPAddr('192.168.0.3',8002)
-    test_emitter=OSCSender('/test',test_ip)
+    emitter=OSCSender('/test',test_ip)
     
-    target_rate=50
     sleep_time=(1000.0/target_rate)/1000.0
     g=Grid(g_size+1,g_size+1)
-    f=FractalHeightmap(g,1,roughness,max_depth=sqrt(g_size))
+    f=FractalHeightmap(g,1,roughness,max_depth=sqrt(g_size))#sqrt because spatial doubling of point data
     while True:
-        g=Grid(g_size+1,g_size+1)
+        #init
         start=datetime.datetime.now()
+        g=Grid(g_size+1,g_size+1)
         f=FractalHeightmap(g,1,roughness,max_depth=sqrt(g_size))
+        #calc
         for y in xrange(0,f.grid.size_y-1):
             for x in xrange(0,f.grid.size_x-1):
-                ###[ test_emitter.message.append(v) for v in [(x,y,n) for n in [ row for row in f.grid.data ]] ]
-                test_emitter.message.clearData()
-                [ test_emitter.message.append(n) for n in ( x,y,f.grid.get(Coord(x,y)) ) ]
-                test_emitter.send()
-                sleep(sleep_time)
+                emitter.message.clearData()
+                [ emitter.message.append(n) for n in ( x,y,f.grid.get(Coord(x,y)) ) ]#thats a tuple not a function call on in.()
+                #send
+                emitter.send()
+                sleep(sleep_time)#roughly approximates target_rate considering sleep is approximate and other execution time. real rate will always be a little slower
+        #render
+        f.grid.render()
+        ##
         end=datetime.datetime.now()
-        print 'Send time',end-start
-        
+        print 'Init+Calc+Send+Render time',end-start
+        print '\n\n'
