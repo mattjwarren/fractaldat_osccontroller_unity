40cd6252eccf3ba201f803302ac5eb10762f82cf
alienbaby <devnull@localhost>
2014-08-12 00:45:21 +0100

    stuff

--- FractalDat.py
+++ FractalDat.py
@@ -114,10 +114,11 @@ class FractalHeightmap(object):
                                       ( int(random.random()*60)+20, int(random.random()*60)+20 ),
                                       ( int(random.random()*60)+20, int(random.random()*60)+20 ) ],
                  
-                 max_depth=3):
+                 max_depth=3,
+                 center_val=None):
         self.grid=grid
         self.max_depth=max_depth
-        self._set_initial_corners(corner_seed_ranges)
+        self._set_initial_corners(corner_seed_ranges,center_val)
         self.roughness=roughness
         self.generate_heightmap([Coord(0,0),###############EDIED OUT MINNUS ONES FRM size parm exp[ressions
                                  Coord(self.grid.size_x-1,0),
@@ -161,7 +162,7 @@ class FractalHeightmap(object):
                                  Coord(self.grid.size_x-1,self.grid.size_y-1)],1
                                 )
 
-    def _set_initial_corners(self,corner_seed_ranges):
+    def _set_initial_corners(self,corner_seed_ranges, center_val=None):
         tl,tr,bl,br=corner_seed_ranges #could be same representation as Corners(), but out of expecteed ranges
         seeds=[[tl,tr],
                [bl,br]]
@@ -174,7 +175,8 @@ class FractalHeightmap(object):
                 except ValueError:
                     val=seeds[x_idx][y_idx]
                 self.grid.make(Coord(x,y),val)
-                
+        if center_val:
+            self.grid.make(Coord(self.grid.size_x/2,self.grid.size_y/2),center_val)
     def generate_heightmap(self,corners,depth):
         '''corners = Corners(Coord(),Coord(),Coord(),Coord()) / tl/tr/bl/br'''
         if depth>self.max_depth: return
@@ -254,7 +256,7 @@ class OSCFractgrid(object):
     
     def __init__(self,grid_size,roughness,zoom,inter_grid_gap,
                   osc_rate,ip_addr,port,target,
-                  starting_corners=None,zoom_steps=4):
+                  starting_corners=None,zoom_steps=4,center_val=None):
         
         self.sleep_time=(1000.0/osc_rate)/1000.0
         self.zoom=zoom
@@ -263,7 +265,7 @@ class OSCFractgrid(object):
         self.grid=Grid(grid_size+1,grid_size+1)
         self.ip_addr=IPAddr(ip_addr,8002)
         self.target=target
-        self.fractal_heightmap=FractalHeightmap(self.grid,1,roughness,max_depth=sqrt(self.grid_size))#sqrt because spatial doubling of point data
+        self.fractal_heightmap=FractalHeightmap(self.grid,1,roughness,max_depth=sqrt(self.grid_size),center_val=center_val)#sqrt because spatial doubling of point data
         self.osc_emitter=OSCSender(self.target,self.ip_addr)
         self.zoom_steps=zoom_steps
         
@@ -279,10 +281,10 @@ class OSCFractgrid(object):
                                                                             ( int(random.random()*60)+20, int(random.random()*60)+20 ),
                                                                             ( int(random.random()*60)+20, int(random.random()*60)+20 ),
                                                                             ( int(random.random()*60)+20, int(random.random()*60)+20 )
-                                                                            ]
+                                                                            ],center_val=center_val
                                                         )#sqrt because spatial doubling of point data
             else:
-                self.fractal_heightmap.zoom(0.75)#parameter is perecnt size of original grid, zoom is a stretched subgrid of that size, centered, and scaled out to fit the
+                self.fractal_heightmap.zoom(self.zoom)#parameter is perecnt size of original grid, zoom is a stretched subgrid of that size, centered, and scaled out to fit the
                                                  #base grid with gaps generated fractally from the points that did exist :::::::::::: generated fractally from the points that did exist  NNEEDDS MOOREE EXPLAANATION
 
             for y in xrange(0,self.fractal_heightmap.grid.size_y):
@@ -309,16 +311,17 @@ class OSCFractgrid(object):
 if __name__ == '__main__': 
     #TESTPROG
     
-    g_size=16
-    roughness=20
+    g_size=32
+    roughness=60
     osc_rate=300 #OSC Messages sent per second
-    zoom=0.75
-    inter_grid_sleep=30
+    zoom=.75
+    inter_grid_sleep=12.33
+    center_val=75
     zoom_steps=4
     ip_addr='77.101.65.99'
     port=8002
     target='/test'
-    osc_fgrid=OSCFractgrid(g_size,roughness,zoom,inter_grid_sleep,osc_rate,ip_addr,port,target,zoom_steps=zoom_steps)
+    osc_fgrid=OSCFractgrid(g_size,roughness,zoom,inter_grid_sleep,osc_rate,ip_addr,port,target,zoom_steps=zoom_steps,center_val=center_val)
     osc_fgrid.start()
     
     
